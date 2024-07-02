db3f1ea4bcbb1b0302e8c5544dec7bf02f584d51
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-07-02 10:25:53 -0600

    -Configuracion de la bd -Creacion de la entidad Contact

---
+++ src/main/java/com/drack/persistence/entities/Contact.java
@@ -0,0 +1,28 @@
+package com.drack.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contacts")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phone;
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
