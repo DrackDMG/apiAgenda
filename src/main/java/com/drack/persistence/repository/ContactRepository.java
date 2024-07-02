ce7090853246c66d57d69a7f14ebfb364608091b
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-07-02 10:27:59 -0600

    -Creacion del repositorio Contact

---
+++ src/main/java/com/drack/persistence/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.drack.persistence.repository;
+
+import com.drack.persistence.entities.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+}
