fd3db5cb6c4e5a10b0e44bb1dd3a75660ac4d3fc
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-07-02 11:22:42 -0600

    -Creacion de datos de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO contacts (name, email, phone, created_at) VALUES ('John Doe', 'jon@gmail.com', '123456789', '2020-01-01 00:00:00');
+INSERT INTO contacts (name, email, phone, created_at) VALUES ('Pablo Jen', 'pabo@gmail.com', '987654321', '2020-01-01 00:00:00');
+INSERT INTO contacts (name, email, phone, created_at) VALUES ('Maria Doe', 'maria@gmail.com', '123456789', '2020-01-01 00:00:00');
+INSERT INTO contacts (name, email, phone, created_at) VALUES ('Luisa Jen', 'luisa@gmail.com', '987654321', '2020-01-01 00:00:00');
